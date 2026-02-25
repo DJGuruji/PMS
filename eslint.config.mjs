4b33b36b463c23898db6008347d58bba7e472eb5
DJGuruji <nath93266@gmail.com>
2026-02-25 21:28:47 +0530

    initial

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,10 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
-import prettierConfig from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  {
-    rules: {
-      "prettier/prettier": "off",
-    },
-  },
-  // Add prettier config at the end to override other rules
-  prettierConfig,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
