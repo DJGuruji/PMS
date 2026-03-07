fcb98f209774f5e0fa142c8846a113c0b41b31a6
DJGuruji <nath93266@gmail.com>
2026-03-07 21:52:37 +0530

    fixed build error

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,21 +1,5 @@
 'use client';
 
-import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
-
 export function Providers({ children }: { children: React.ReactNode }) {
-  const siteKey = process.env.NEXT_PUBLIC_GOOGLE_RECAPTCHE_SITEKEY || '';
-  
-  return (
-    <GoogleReCaptchaProvider 
-      reCaptchaKey={siteKey}
-      scriptProps={{
-        async: true,
-        defer: true,
-        appendTo: 'head',
-        nonce: undefined,
-      }}
-    >
-      {children}
-    </GoogleReCaptchaProvider>
-  );
+  return <>{children}</>;
 }
